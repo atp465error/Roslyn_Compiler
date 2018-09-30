3ed7674240b5067015e03e36dcec43afc78ca836
Antlion <40786755+r3xq1@users.noreply.github.com>
2018-09-30 15:46:11 +0300

    Add files via upload

---
+++ RoslynCompiler/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RoslynCompiler")]
+[assembly: AssemblyProduct("RoslynCompiler")]
+[assembly: AssemblyCopyright("Copyright © Antlion [XakFor.Net]  2018")]
+[assembly: ComVisible(false)]
+[assembly: Guid("33f43f9b-643b-401e-978f-7c9f496fc061")]
+[assembly: AssemblyVersion("0.0.0.5")]
+[assembly: AssemblyFileVersion("0.0.0.5")]
